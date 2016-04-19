3e178f2d7ae3a1270ff26ae9c5d33f29bf4befda
Vladimir Podolyan <dreamcore2219@gmail.com>
2016-04-19 11:18:00 +0300

    Added sample xml structure

--- create-farx.js
+++ create-farx.js
@@ -1,13 +1,28 @@
+var fs = require('fs');
 var data2xml = require('data2xml');
 var convert = data2xml({xmlheader: '<?xml version="1.0" standalone="yes" ?>\n'});
+
+const defaultFiddlerVer = "4.6.2.3";
 
 var xml = convert(
   "AutoResponder",
   {
-    SimpleData : 'Simple Value',
-    ComplexData: {
-      _attr: { version: "4.6"}
+    _attr: { LastSave: new Date().toISOString(), FiddlerVersion: "4.6.2.3"  },
+    State: {
+      _attr: { Enabled: false, Fallthrough: false, UseLatency: false },
+      ResponseRule: {
+        _attr: {
+          Match: "https://www-test.bmednet.it/lr/html/js/med/nac/conversione/fondi_terzi/fondi_terzi.js",
+          Action: "C:\\DevRootMediolanum\\Projects\\lr\\html\\js\\med\\nac\\conversione\\fondi_terzi\\fondi_terzi.js",
+          Enabled: false
+        }
+      }
     }
 });
 
 console.log(xml);
+
+fs.writeFile("auto-response.xml", xml, (err) => {
+  if (err) throw err;
+  console.log("File has been saved!");
+});
