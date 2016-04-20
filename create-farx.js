225db3fdaef337fc037e26df2aa40bf64f97da5f
Vladimir Podolyan <dreamcore2219@gmail.com>
2016-04-20 18:24:35 +0300

    Asynchronous logic

--- create-farx.js
+++ create-farx.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var util = require('util');
 var url = require('url');
 var data2xml = require('data2xml');
-var rulesFactory = require('./rulesFactory.js');
+var prepareRules = require('./prepareRules.js');
 
 if (process.argv.length < 3) {
   console.log("Please specify path to a directory with js files ");
@@ -23,27 +23,27 @@ const baseUrl = util.format("%s", process.argv[3]);
 console.log("BASE URL = " + baseUrl);
 
 // prepare js files
-rulesFactory(filesPath, baseUrl, (err, rules) => {
-  console.log("rules", rules);
-});
-
-// Rewrite following code in asynchronous way
-var rules = [];
-var convert = data2xml({xmlheader: '<?xml version="1.0" standalone="yes" ?>\n'});
-var xml = convert(
-  "AutoResponder",
-  {
-    _attr: { LastSave: new Date().toISOString(), FiddlerVersion: "4.6.2.3"  },
-    State: {
-        _attr: { Enabled: false, Fallthrough: false, UseLatency: false },
-        ResponseRule: rules
-      }
-    }
-);
-
-console.log(xml);
-
-fs.writeFile("auto-response.farx", xml, (err) => {
+prepareRules(filesPath, baseUrl, (err, rules) => {
   if (err) throw err;
-  console.log("File has been created!");
+
+  console.log("Rules = ", rules);
+
+  var convert = data2xml({xmlheader: '<?xml version="1.0" standalone="yes" ?>\n'});
+  var xml = convert(
+    "AutoResponder",
+    {
+      _attr: { LastSave: new Date().toISOString(), FiddlerVersion: "4.6.2.3"  },
+      State: {
+          _attr: { Enabled: false, Fallthrough: false, UseLatency: false },
+          ResponseRule: rules
+        }
+      }
+  );
+
+  console.log("XML = ", xml);
+
+  fs.writeFile("auto-response.farx", xml, (err) => {
+    if (err) throw err;
+    console.log("File has been created!");
+  });
 });
