26101a9317b1724382b835b51a56ca3fe4cf5d24
Vladimir Podolyan <dreamcore2219@gmail.com>
2016-04-21 16:45:09 +0300

    Added posibility to add files from subfolders

--- create-farx.js
+++ create-farx.js
@@ -7,43 +7,55 @@ var data2xml = require('data2xml');
 var prepareRules = require('./prepareRules.js');
 
 if (process.argv.length < 3) {
-  console.log("Please specify path to a directory with js files ");
-  return;
+    console.log("Please specify path to a directory with js files ");
+    return;
 }
 
 if (process.argv.length < 4) {
-  console.log("Please specify rule url to be matched");
-  return;
+    console.log("Please specify rule url to be matched");
+    return;
 }
 
 const defaultFiddlerVer = "4.6.2.3";
 const filesPath = process.argv[2];
 const baseUrl = util.format("%s", process.argv[3]);
+const recursively = process.argv[4] == "-r" ? true : false;
 
 console.log("BASE URL = " + baseUrl);
 
-// prepare js files
-prepareRules(filesPath, baseUrl, (err, rules) => {
-  if (err) throw err;
-
-  console.log("Rules = ", rules);
-
-  var convert = data2xml({xmlheader: '<?xml version="1.0" standalone="yes" ?>\n'});
-  var xml = convert(
-    "AutoResponder",
-    {
-      _attr: { LastSave: new Date().toISOString(), FiddlerVersion: "4.6.2.3"  },
-      State: {
-          _attr: { Enabled: false, Fallthrough: false, UseLatency: false },
-          ResponseRule: rules
-        }
-      }
-  );
-
-  console.log("XML = ", xml);
-
-  fs.writeFile("auto-response.farx", xml, (err) => {
+prepareRules(filesPath, baseUrl, recursively, (err, rules) => {
     if (err) throw err;
-    console.log("File has been created!");
-  });
+
+    console.log("Rules = ", rules);
+
+    var xml = getXML(rules, (err, rules) => {
+        if (err) throw err;
+
+        console.log("XML = ", xml);
+
+        fs.writeFile("auto-response.farx", xml, (err) => {
+            if (err) throw err;
+            console.log("auto-response.farx has been created!");
+        });
+    });
 });
+
+function getXML(rules, doneCallback) {
+    var convert = data2xml({xmlheader: '<?xml version="1.0" standalone="yes" ?>\n'});
+    try {
+        var xml = convert(
+            "AutoResponder",
+            {
+                _attr: { LastSave: new Date().toISOString(), FiddlerVersion: "4.6.2.3"  },
+                State: {
+                    _attr: { Enabled: false, Fallthrough: false, UseLatency: false },
+                    ResponseRule: rules
+                }
+            }
+        );
+        doneCallback(undefined, xml);
+    }
+    catch (err){
+        doneCallback(err);
+    }
+}
